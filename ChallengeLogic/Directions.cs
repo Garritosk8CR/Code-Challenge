608731eb963e4e35053992a1da73d17fb6466fc7
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2024-10-15 07:02:40 -0700

    Helpers for day 3 part 2

---
+++ ChallengeLogic/Directions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeLogic;
+
+
+public static class Directions
+{
+    public static Point[] WithoutDiagonals { get; } =
+    [
+        (0, 1),
+        (1, 0),
+        (0, -1),
+        (-1, 0),
+    ];
+
+    public static Point[] WithDiagonals { get; } =
+    [
+        (0, 1),
+        (1, 0),
+        (0, -1),
+        (-1, 0),
+        (1, 1),
+        (-1, 1),
+        (1, -1),
+        (-1, -1)
+    ];
+
+    public static Point3d[] WithoutDiagonals3d { get; } =
+    [
+        (1, 0, 0),
+        (0, 1, 0),
+        (0, 0, 1),
+        (-1, 0, 0),
+        (0, -1, 0),
+        (0, 0, -1),
+    ];
+}
